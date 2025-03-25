758317abf70eafe442528aac4529667d9f76a1dc
zeythar <zeythar@protonmail.com>
2025-03-25 04:39:09 +0100

    update cors origin to gh pages

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 3001;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://csstatspy.github.io/csstatspy.web/",
   methods: "GET,POST",
 };
 
